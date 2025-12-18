a5cc1a5c1b6e406f76c1bc56aba47873b180025e
IIAydedeII <ahmetyasindd@gmail.com>
2025-12-18 16:12:44 +0300

    Added 5th variant "text" to button

--- components/atoms/Button.tsx
+++ components/atoms/Button.tsx
@@ -1,8 +1,7 @@
-import { css, LinariaClassName } from "@linaria/core";
-import { styled } from "@linaria/react";
 import React from "react";
+import { styled } from "@linaria/react";
 
-type Variant = "solid" | "outlined" | "dashed" | "light";
+type Variant = "solid" | "outlined" | "dashed" | "light" | "text";
 
 type ButtonProps = {
   children: React.ReactNode;
@@ -23,53 +22,58 @@ const StyledButton = styled.button`
   transition-duration: var(--transition-duration);
   transition-timing-function: var(--transition-function);
   &[data-variant="solid"] {
-  color: oklch(from var(--theme) calc((1 - sign(l - 0.7)) / 2) 0 0);
-  background-color: var(--theme);
-  border-color: var(--theme);
-  box-shadow: 0 2px 0 oklch(from var(--theme) l c h / 0.4);
-  &:hover {
-    background-color: var(--theme-hover);
-    border-color: var(--theme-hover);
-  }
-  &:active {
-    background-color: var(--theme-active);
-    border-color: var(--theme-active);
-    box-shadow: none;
-    transform: translateY(1px);
-  }
+    color: oklch(from var(--theme) calc((1 - sign(l - 0.7)) / 2) 0 0);
+    background-color: var(--theme);
+    border-color: var(--theme);
+    box-shadow: 0 2px 0 oklch(from var(--theme) l c h / 0.4);
+    &:hover {
+      background-color: var(--theme-hover);
+      border-color: var(--theme-hover);
+    }
+    &:active {
+      background-color: var(--theme-active);
+      border-color: var(--theme-active);
+      box-shadow: none;
+      transform: translateY(1px);
+    }
   }
   &[data-variant="outlined"],
   &[data-variant="dashed"] {
-  color: var(--theme);
-  background: var(--background);
-  border-color: currentColor;
-  &:hover {
-    color: var(--theme-hover);
-  }
-  &:active {
-    color: var(--theme-active);
-  }
+    color: var(--theme);
+    background: var(--background);
+    border-color: currentColor;
+    &:hover {
+      color: var(--theme-hover);
+    }
+    &:active {
+      color: var(--theme-active);
+    }
     &[data-variant="dashed"] {
       border-style: dashed;
     }
   }
-  &[data-variant="light"] {
-  background-color: color-mix(in oklch, var(--theme), 70% var(--background));
-  border-color: color-mix(in oklch, var(--theme), 70% var(--background));
-  &:hover {
-    background-color: color-mix(
-      in oklch,
-      var(--theme),
-      calc(70% - var(--accent)) var(--background)
-    );
-  }
-  &:active {
-    background-color: color-mix(
-      in oklch,
-      var(--theme),
-      calc(70% + var(--accent)) var(--background)
-    );
-  }
+  &[data-variant="light"],
+  &[data-variant="text"] {
+    background-color: color-mix(in oklch, var(--theme), 70% var(--background));
+    border-color: color-mix(in oklch, var(--theme), 70% var(--background));
+    &[data-variant="text"] {
+      background-color: transparent;
+      border-color: transparent;
+    }
+    &:hover {
+      background-color: color-mix(
+        in oklch,
+        var(--theme),
+        calc(70% - var(--accent)) var(--background)
+      );
+    }
+    &:active {
+      background-color: color-mix(
+        in oklch,
+        var(--theme),
+        calc(70% + var(--accent)) var(--background)
+      );
+    }
   }
   &[data-block="true"] {
     width: 100%;
